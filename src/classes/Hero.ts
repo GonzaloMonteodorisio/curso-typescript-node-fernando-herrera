0fa01caf27e0f268bb0ca6c0c24cf5dcec8fda5a
GonzaloMonteodorisio <gonzalomonteodorisio@gmail.com>
2025-05-19 21:43:18 -0300

    exports and imports

---
+++ src/classes/Hero.ts
@@ -0,0 +1,9 @@
+export class Hero {
+  constructor(
+    public name: string,
+    public powerid: number,
+    public age: number,
+  ) {}
+}
+
+export class Hero2 {}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-
+import { Hero, Hero2 } from './classes/Hero';
 
 console.log('Hola Mundo!!!!');
+
+const ironMan = new Hero('Iron Man', 1, 55);
+
+console.info('ironMan: ', ironMan);
 
 
 
