d2cb99dd4d7d6d9b92ae9998aa742211853eec52
GonzaloMonteodorisio <gonzalomonteodorisio@gmail.com>
2025-05-19 22:00:35 -0300

    export default and export wiyh alias

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // import { Hero as SuperHero, Hero2 } from './classes/Hero';
 
 import * as HeroClasses from './classes/Hero';
-import powers, { Power } from './data/powers'; // como es importación por defecto puedo importarlo con el nombre que quiera
+import powers2, { Power } from './data/powers'; // como es importación por defecto puedo importarlo con el nombre que quiera
 
 const Hero = 123
 
@@ -9,7 +9,8 @@ const ironMan = new HeroClasses.Hero('Iron Man', 1, 55);
 
 console.info('ironMan: ', ironMan);
 
-console.info('powers: ', powers);
+console.info('powers2: ', powers2);
+
 
 
 
