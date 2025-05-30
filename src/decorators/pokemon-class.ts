1fd3305163aa4c265c0ccdf54f777e07d8c7821b
GonzaloMonteodorisio <gonzalomonteodorisio@gmail.com>
2025-05-29 21:48:36 -0300

    factory decorators

--- src/decorators/pokemon-class.ts
+++ src/decorators/pokemon-class.ts
@@ -2,7 +2,16 @@ function printToConsole( constructor: Function ) {
   console.info('constructor: ', constructor);
 }
 
-@printToConsole
+// Factory constructor -> Debe retornar una función
+const printToConSoleConditional = ( print: boolean = false ): Function => {
+  if ( print ) {
+    return printToConsole
+  }
+
+  return () => console.info('Hello world')
+}
+
+@printToConSoleConditional(true)
 export class Pokemon {
 
   public publicApi: string = 'https://pokeapi.co';
