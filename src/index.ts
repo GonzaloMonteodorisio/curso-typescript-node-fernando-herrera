b4e5c479453648f03a49705d84d23e86ab537ff9
GonzaloMonteodorisio <gonzalomonteodorisio@gmail.com>
2025-05-29 21:38:08 -0300

    class decorators

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { getPokemon } from "./generics/get-pokemon";
+import { Pokemon } from './decorators/pokemon-class'
+
+
+// GENERICS 
 
 getPokemon(4)
   .then ( pokemon => console.info('pokemon.sprites.front_default: ', pokemon.sprites.front_default))
   .catch( error => console.error('error: ', error))
   .finally(() => console.info ('End of getPokemon'))
+
+
+// DECORATORS
+const charmander = new Pokemon('Charmander')
+
+console.info('charmander: ', charmander)
